c90c45b5a16baaf26212b801d654ea3b62d83388
Vineel Kumar Veludandi <63500509+vineel49@users.noreply.github.com>
2020-05-18 17:01:19 +0530

    Delete Get_Trellis.m

--- Get_Trellis.m
+++
@@ -1,7 +0,0 @@
-% Trellis for M = 2, L=2
-function [P_State,P_State_trans,P_Ip,Ga_Inx]= Get_Trellis()
-P_State = [1,2;3,4;1,2;3,4]; % PREVIOUS STATES
-P_State_trans =  [1,3,1,3;2,4,2,4]; % TRANSPOSE OF P_State
-P_Ip = [1,1,2,2;1,1,2,2]; % PREVIOUS INPUTS
-Ga_Inx = [1,2;3,4;5,6;7,8];% GAMMA INDICES FOR THE ALPHA RECURSION (THINK OF BCJR ALGORITHM)
-end
